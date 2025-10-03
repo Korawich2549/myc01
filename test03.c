4718049fa5d49e3fcc5080fee338a9b33780feb9
Korawich2549 <jamekunza123@gmail.com>
2025-10-03 12:13:50 +0700

    first commit

---
+++ test03.c
@@ -0,0 +1,26 @@
+//โปรแกรมแสดงข้อความ Iot-SAU
+//แสดงผลการคำนวณของ 2 ยกกำลัง 500
+// " " double quote ' ' single quote,# hash, / slash, \ back slash
+//{} curly bracket, () round bracket , [] square bracket , <> angle bracket
+// : colon, ; semicolon, ` backtick | pipe, & ampersand
+
+
+#include <stdio.h> //หรือ "stdio.h"
+#include <math.h>
+
+//การกำหนดค่าคงที่ คือ การสร้างตัวแทนเพื่อมาเก็บข้อมูลใดๆ โดยที่ไม่สารมารถเปลี่ยนค่าข้อมูลนั้นได้
+//และสามารถเอาตัวแทนที่สร้างไปใช้โปรแกรมได้
+#define showline printf("+++++++++++++++++++++++\n");
+#define SMILE "^____^ 5555"
+
+int main (){
+    showline
+    printf("Iot-SAU\n");
+    showline
+    showline
+    printf("%lf\n", pow(2,10) );
+    printf("%s\n", SMILE);
+    showline
+    
+    return 0;
+}
---
+++ test04.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+int main(){
+
+    //คำสั่ง printf() แสดงข้อมูลที่อยู่ใน "" แรกเท่านั้นทางหน้าจอ
+    //แต่หากมีอะไรที่อยู่นอกเหนือจาก "" แรก
+    //ข้อมูลเหล่านั้นจะต้องถูกนำมาแสดงที่ "" แรกด้วย Format Spec./Format Code เสมอ
+    printf("AAA...%d ----%s ^_^ ",111, "wow wow wow",10 + 20);
+
+
+    printf("AAA\nBBB\n\n\nCCC\tDDD\t\tCCC");
+
+
+    return 0;
+}
